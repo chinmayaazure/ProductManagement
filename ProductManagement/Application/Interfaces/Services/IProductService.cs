998d1cb8e99c9034701c38416d6c4e13e682ff4c
chinmayaazure <chinmaya.it@gmail.com>
2026-05-04 16:50:06 +0530

    Add project files.

---
+++ ProductManagement/Application/Interfaces/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using ProductManagement.Application.DTOs;
+namespace ProductManagement.Application.Interfaces.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetAllProductsAsync();
+        Task<ProductDTO?> GetProductByIdAsync(int id);
+        Task<ProductDTO?> AddProductAsync(CreateProductDTO productDto);
+        Task UpdateProductAsync(UpdateProductDTO productDto);
+        Task DeleteProductAsync(int id);
+    }
+}
---
+++ ProductManagement/Program.cs
@@ -0,0 +1,61 @@
+
+using Microsoft.EntityFrameworkCore;
+using ProductManagement.Application.Interfaces.Repositories;
+using ProductManagement.Application.Interfaces.Services;
+using ProductManagement.Application.Services;
+using ProductManagement.Infrastructure.Data;
+using ProductManagement.Infrastructure.Repositories;
+
+namespace ProductManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                //Disable Camel case naming conventions for JSON Serialization and Deserialization
+                options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            });
+
+
+
+            // Add DbContext with SQL Server
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+
+            // Register services and repositories
+            builder.Services.AddScoped<IProductRepository, ProductRepository>();
+            builder.Services.AddScoped<IProductService, ProductService>();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
